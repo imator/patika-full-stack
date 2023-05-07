6f2873b3660f42046c8305cdbd0bc54ab226ba7a
Ahmet ORMAN <a.orman37@gmail.com>
2023-05-07 16:11:43 +0300

    ÖDEV Artık YIl Hesabı

---
+++ src/ArtikYilHesabi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArtikYilHesabi {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int year;
+        System.out.print("Yıl Giriniz :");
+        year = inp.nextInt();
+
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                System.out.println(year + " bir artık yıldır !");
+            } else {
+                System.out.println(year + " bir artık yıl değildir !");
+            }
+        } else {
+            if ((year % 4 == 0)) {
+                System.out.println(year + " bir artık yıldır !");
+            } else {
+                System.out.println(year + " bir artık yıl değildir !");
+            }
+        }
+    }
+}
